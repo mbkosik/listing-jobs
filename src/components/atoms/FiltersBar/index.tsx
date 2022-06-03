35e552d31247e71c4043c758a0b0d56177c78cd8
mbkosik <72941144+mbkosik@users.noreply.github.com>
2022-06-03 10:27:07 +0200

    add clearFilters button

--- src/components/atoms/FiltersBar/index.tsx
+++ src/components/atoms/FiltersBar/index.tsx
@@ -1,16 +1,23 @@
 import { FC } from "react";
-import { useJobStore } from "../../../context/job.context";
+import { ActionType, useJobStore } from "../../../context/job.context";
 import { FilterButton } from "../FilterButton";
 import styles from "./FiltersBar.module.scss";
 
 export const FiltersBar: FC = () => {
-  const { state } = useJobStore();
+  const { state, dispatch } = useJobStore();
+
+  const clearFilters = () => {
+    dispatch({
+      type: ActionType.RESET
+    })
+  }
 
   return (
     <div className={styles.wrapper}>
       {state.activeFilters.map((filter, index) => (
         <FilterButton filter={filter} key={index} />
       ))}
+      <button className={styles.clearButton} onClick={clearFilters}>Clear</button>
     </div>
   );
 };
