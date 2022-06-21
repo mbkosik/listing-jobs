0ae9a8f6ae21cadd784b382a438cc71a1b1d08c5
mbkosik <72941144+mbkosik@users.noreply.github.com>
2022-06-21 09:57:09 +0200

    display FiltersBar if filters are active

--- src/components/organisms/JobList/index.tsx
+++ src/components/organisms/JobList/index.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
-import styles from "./JobList.module.scss";
+
+import { useJobStore } from "context/job.context";
 import { FiltersBar } from "components/atoms/FiltersBar";
 import { JobItem } from "components/atoms/JobItem";
 import { JOB_DATA } from "data/data";
 
+import styles from "./JobList.module.scss";
+
 export const JobList: FC = () => {
+  const { state } = useJobStore();
+
   return (
     <div className={styles.wrapper}>
-      <FiltersBar />
+      {!!state.activeFilters.length && <FiltersBar />}
       {JOB_DATA.map((job) => (
         <JobItem
           job={job}
